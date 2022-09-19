e7cc682216f856bcb0d0b146c139d95a69bf23d4
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-09-19 18:03:18 -0500

    cambios

--- resources/views/livewire/admin/curriencies.blade.php
+++ resources/views/livewire/admin/curriencies.blade.php
@@ -15,14 +15,15 @@
                     </div>
                     <div>
                         <x-jet-label value="MXN" />
-                        <x-jet-input type="text" wire:model="mxn" class="w-full"/>
+                        <x-jet-input type="text" wire:model="mxn" class="w-full bg-gray-100" disabled/>
                     </div>
                 </div>
+                <p class="p-4 text-gray-400 text-sm">Tipo de cambio pesos por dolar E.U.A interbancario a 48 hrs apertura <a href="https://www.banxico.org.mx/SieAPIRest/service/v1/doc/catalogoSeries#" class="hover:text-gray-900"><b> fuente Banco de México </b></a></p>
                 <div class="flex justify-end p-4 items-center">
                     <x-jet-action-message class="mr-3 text-green-500" on="updated">
                         Moneda actualizada
                     </x-jet-action-message>
-                    <x-jet-button wire:click="update">
+                    <x-jet-button wire:click="update" disabled>
                         Actualizar
                     </x-jet-button>
                 </div>
