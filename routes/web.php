3213d83138529fda496be4f9983bcaab8b436271
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-11-05 18:27:25 -0600

    cambios

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Models\Order;
 use App\Models\Reference;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Password;
@@ -211,4 +212,13 @@ Route::get('/dolar',function(){
         }
     }
     return $valor;
+});
+
+
+Route::get('/app-down', function () {
+    Artisan::call('down');
+});
+
+Route::get('/app-up', function () {
+    Artisan::call('up');
 });
